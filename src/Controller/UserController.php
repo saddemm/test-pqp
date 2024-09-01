a6732cdc83f7add18cae615e3f1c4880644e82a1
saddemm <saddem.anane@gmail.com>
2024-09-01 15:18:25 +0100

    Simple user auth

---
+++ src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use http\Client\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/login', name: 'app_user', methods: ['POST'])]
+    public function index(Request $request, UserRepository $userRepository): Response
+    {
+        // Récupérer les données de la requête POST
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        $user = $userRepository->findOneBy(['email' => $email, 'password' => $password]);
+
+        return $user ? new Response('ok') : new Response('ko');
+    }
+}
