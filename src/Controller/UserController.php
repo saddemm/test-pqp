caf0f3b0ef43e7949a4d6f94b7e8a25404a28ca9
saddemm <saddem.anane@gmail.com>
2024-09-01 15:26:38 +0100

    fix use client

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
-use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
